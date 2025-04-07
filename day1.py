8387852bf0deab013e499db92e8b4a2df3600683
Max Carvajal <jcarvajal288@gmail.com>
2025-04-07 16:06:29 -0500

    Day 1 part 2 complete

--- day1.py
+++ day1.py
@@ -8,4 +8,12 @@ def total_distance_between_lists(filename):
     right_side = sorted([int(x[-1]) for x in splits])
     differences = [abs(x[1] - x[0]) for x in zip(left_side, right_side)]
     return sum(differences)
-    
+
+
+def similarity_score(filename):
+    splits = [x.split(' ') for x in read_lines_from_file(filename)]
+    left_side = sorted([int(x[0]) for x in splits])
+    right_side = sorted([int(x[-1]) for x in splits])
+    similarity_scores = [right_side.count(x) * x for x in left_side]
+    return sum(similarity_scores)
+        
--- day1test.py
+++ day1test.py
@@ -9,6 +9,13 @@ class TestDay1(unittest.TestCase):
     def test_part1_full(self):
         self.assertEqual(day1.total_distance_between_lists('./data/day1_fulldata.txt'), 2769675)
 
+    def test_part1_part2_sample(self):
+        self.assertEqual(day1.similarity_score('./data/day1_sampledata.txt'), 31)
+
+    def test_part1_part2_full(self):
+        self.assertEqual(day1.similarity_score('./data/day1_fulldata.txt'), 31)
+
+
 if __name__ == '__main__':
     unittest.main()
         
