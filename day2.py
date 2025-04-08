db7a2db06bd460bea9ecc360543aa24d11838ec2
Max Carvajal <jcarvajal288@gmail.com>
2025-04-08 15:40:49 -0500

    Day 2 part 2 complete

--- day2.py
+++ day2.py
@@ -3,6 +3,7 @@ import util
 def _parse_report(report):
     return [int(n) for n in filter(None, report.split(' '))]
 
+
 def _is_safe(report):
     is_increasing = report[0] < report[1]
     if is_increasing:
@@ -17,9 +18,27 @@ def _is_safe(report):
                 return False
     return True
 
+
 def count_safe_reports(filename):
     reports = util.read_lines_from_file(filename)
     parsed_reports = [_parse_report(report) for report in reports]
     safety_reports = [_is_safe(report) for report in parsed_reports]
+    return safety_reports.count(True)
+
+
+def _is_safe_with_dampener(report):
+    if _is_safe(report):
+        return True
+    for i in range(len(report)):
+        sliced_report = report[:i] + report[i+1:]
+        if _is_safe(sliced_report):
+            return True
+    return False
+
+
+def count_safe_reports_with_dampener(filename):
+    reports = util.read_lines_from_file(filename)
+    parsed_reports = [_parse_report(report) for report in reports]
+    safety_reports = [_is_safe_with_dampener(report) for report in parsed_reports]
     return safety_reports.count(True)
             
--- day2test.py
+++ day2test.py
@@ -9,6 +9,12 @@ class TestDay2(unittest.TestCase):
     def test_part1_full(self):
         self.assertEqual(day2.count_safe_reports('./data/day2_fulldata.txt'), 332)
 
+    def test_part2_sample(self):
+        self.assertEqual(day2.count_safe_reports_with_dampener('./data/day2_sampledata.txt'), 4)
+
+    def test_part2_full(self):
+        self.assertEqual(day2.count_safe_reports_with_dampener('./data/day2_fulldata.txt'), 398)
+
 
 if __name__ == '__main__':
     unittest.main()
